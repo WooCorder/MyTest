5c6b2ecf5a879b682c2baf0a4731e1687dcdc275
WooCorder <chivarly45@gmail.com>
2018-03-16 15:12:30 +0900

    두번째 테스트 commit

--- main.cpp
+++ main.cpp
@@ -6,45 +6,54 @@
 #include <opencv2/imgproc.hpp>
 
 
+#define DIFFERENCE_FRAME_THRESHOLD 3
+#define FILE_NAME "1.mp4"
+
 using namespace std;
 using namespace cv;
 
-Mat tracking(Mat p_mat);
 
-int main()
+Mat ballTracking(Mat p_mat);
+
+void main()
 {
-	VideoCapture vc("1.mp4");
+	VideoCapture first_vc("1.mp4");
+	VideoCapture second_vc("1.mp4");
+	VideoCapture third_vc("1.mp4");
 
-	Mat mainImg;
+	Mat first_img, second_img, third_img;
+	int differenceFrame = 0;
 
-	while(1)
+	for (int frameCount = 0; first_img.data != NULL; frameCount++)
 	{
-		vc >> mainImg;
-		if (mainImg.empty()) break;
-		
-		resize(mainImg, mainImg, Size(), 0.5, 0.5);//scalex, scaley
+		first_vc >> first_img;
 
-		mainImg = tracking(mainImg);
+		if (frameCount > DIFFERENCE_FRAME_THRESHOLD)
+			second_vc >> second_img;
 
+		if (frameCount > DIFFERENCE_FRAME_THRESHOLD * 2)
+			third_vc >> third_img;
 
-		imshow("Ball Tracking", mainImg);
-		if (waitKey(1) == 27) break; //ESC
+		imshow("pre video", first_img);
+		imshow("video", second_img);
 
+		if (waitKey(10) == 27) break; //ESC
 	}
-	
-	
 
+	destroyAllWindows();
+
+	return;
 }
 
 
-Mat tracking(Mat p_mat)
+
+Mat ballTracking(Mat p_mat)
 {
+	//for (int frameCount = 0; after_frame.data != NULL; frameCount++)
+	{
 
-
-
-
-
-
+	}
 
 	return p_mat;
+
 }
